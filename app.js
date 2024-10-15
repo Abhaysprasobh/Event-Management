324182f12426c585193d1a3e0fdf4618754a22fa
Underemployed <underemployed871@gmail.com>
2024-10-15 20:13:22 +0530

    restructured app.js

--- app.js
+++ app.js
@@ -1,75 +1,50 @@
+// modules
+require('dotenv').config();
 const express = require('express');
-const fileUpload = require("express-fileupload")
+const fileUpload = require('express-fileupload');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
-const { MongoClient } = require('mongodb');
-require("dotenv").config();
-
-const fs = require('fs');
-const path = require("path");
-
-const routes = require("./routes/routes");
-const connect = require("./db/db_connection");
-
-
-// models
-const User = require("./models/User");
 const morgan = require('morgan');
+const path = require('path');
 
+// functions and models
+const routes = require('./routes/routes');
+const connect = require('./db/db_connection');
+const User = require('./models/User');
 
-// server
 const app = express();
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(morgan("dev"));
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+const port = process.env.PORT || 3000;
 
-// session 
+// Middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 app.use(session({
-    secret: 'idkwwwsakdsasdkhjadkbhfdasfdnb',
+    secret: process.env.SESSION_SECRET || 'idkwwwsakdsasdkhjadkbhfdasfdnb',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // http: false
+    cookie: { secure: false }
 }));
-
 app.use(fileUpload({
     useTempFiles: true,
     createParentPath: true,
     tempFileDir: path.join(__dirname, 'tmp'),
-    limits: { fileSize: 4 * 1024 * 1024 } 
+    limits: { fileSize: 4 * 1024 * 1024 }
 }));
 
+// View engine
+app.set('view engine', 'ejs');
+
+// Routes
 app.use(routes);
 
-
-
-// urls
-const url = process.env.url || "mongodb://localhost:27017/event-management";
-const db_name = process.env.db_name || "event-management";
-const port = process.env.port || 3000;
-
-// connect to mongo
-connect().then(() => {
-    app.listen(port, () => {
-        console.log(`Server is running at http://localhost:${port}`);
+// Database connection and server start
+connect()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running at http://localhost:${port}`);
+        });
+    })
+    .catch(error => {
+        console.error('Failed to connect to the database:', error);
     });
-}).catch(error => {
-    console.error('Failed to connect to the database:', error);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
